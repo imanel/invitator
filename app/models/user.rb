094241c2688d9e4787be3d8e2abc193eca45fc52
Bernard Potocki <bernard.potocki@imanel.org>
2011-03-19 19:07:11 +0100

    Devise basicd

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, :registerable
+         #:recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :login, :password, :password_confirmation
+  
+  validates_presence_of :login
+  validates_uniqueness_of :login
+  validates_format_of :login, :with => /^[\w\d]+$/, :message => "only letter, number or underscore allowed"
+  
+  validates_presence_of :password
+  validates_confirmation_of :password
+  validates_length_of :password, :within => 3..20
+  
+end

4149b012fe0ee813450e242702288b6728e4585c
Bernard Potocki <bernard.potocki@imanel.org>
2011-03-19 18:21:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SageTestProject::Application.config.secret_token = '723b3163f022446535637e39b62251d3de0d40d352812d715070d0cb8850049cf877667d084dd75f81d2539ebc71248c7ef5a604687c8d9dddf9e863e2432c66'
